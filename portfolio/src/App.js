57d9d85c2a43a98bfc8d29cecd34ca5b52e297e8
ReaganJohnston <hyerx01@gmail.com>
2021-02-09 23:50:54 +1000

    Major updates on the website

---
+++ portfolio/src/App.js
@@ -0,0 +1,76 @@
+import './App.css';
+import React from "react"
+
+// reactstrap components
+import {
+    Button,
+    Label,
+    FormGroup,
+    Input,
+    NavItem,
+    NavLink,
+    Nav,
+    TabContent,
+    TabPane,
+    Container,
+    Row,
+    Col,
+} from "reactstrap";
+
+
+
+function App() {
+  return (
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    {/*}
+    <header id="header">
+    <div class="header container">
+        <div class="header-breadcrumb">
+        </div>
+        <div class="header-nav">
+            <ul>
+                <li>
+                    <a href="/" title="home">./rj</a>
+                </li>
+                <li>
+                    <a href="/cyber" title="cyber-security">./cyber</a>
+                </li>
+                <li>
+                    <a href="/work" title="selected-projects">./work</a>
+                </li>
+                <li>
+                    <a href="/about" title="about-me">./about</a>
+                </li>
+                <li>
+                    <a href="/contact" title="contact-me">./contact</a>
+                </li>
+                <li class="social-media">
+                    <a href="!!have git-hub link!!" target="_blank" title="git-hub"></a>
+                </li>
+                <li class="social-media">
+                    <a href="!!have linked-in link!!" target="_blank" title="linked-in"></a>
+                </li>
+            </ul>
+        </div>
+    </div>
+  </header> */}
+  );
+}
+
+export default App;
---
+++ portfolio/src/components/navbar.js
@@ -0,0 +1,165 @@
+/*
+=========================================================
+* Coded by Reagan Johnston
+* Utilising Paper Kit React - v1.2.0 frameworks
+=========================================================
+*/
+
+import React from "react"
+import { Link } from "react-router-dom"
+import classnames from "classnames"
+
+import {
+  Collapse,
+  NavbarBrand,
+  Navbar,
+  NavItem,
+  NavLink,
+  Nav,
+  Containter
+} from "reactstrap";
+
+function NavBar(){
+  //cosntants used across the whole function
+  const [navCollapse, setNavCollapse] = React.useState(false);
+  const [navColour, setNavColour] = React.useState("navbar-transparent");
+
+  const toggleCollapse = () => {
+    document.documentElement.classList.toggle("nav-open");
+    setNavCollapse(!navCollapse);
+  }
+
+
+  /*  code imported from kit-react, updates and changes the nav size depending on the size
+  of the window opened    */
+  
+  React.useEffect(() => {
+  const updateNavbarColor = () => {
+    if (
+      document.documentElement.scrollTop > 299 ||
+      document.body.scrollTop > 299
+    )
+    
+    {setNavbarColor("");} 
+    
+    else if (
+      document.documentElement.scrollTop < 300 ||
+      document.body.scrollTop < 300
+    ) 
+    
+    {setNavbarColor("navbar-transparent");}
+  };
+
+  window.addEventListener("scroll", updateNavbarColor);
+
+  return function cleanup() {
+    window.removeEventListener("scroll", updateNavbarColor);
+  };
+  });
+
+
+  return(
+    <NavBar
+      className={classnames("fixed-top", navColour)}
+      expand="lg"
+      color-on-scroll="300"
+    >
+      <Container>
+        <div className="translate">
+          {/* make sure to update the /index page to an actual real address*/}
+          <NavbarBrand
+            data-placement="bottom"
+            to="/home"
+            target="_blank"
+            title="Return Home"
+            tag={Link}
+          >Reagan Johnston</NavbarBrand>
+
+          <button
+            aria-expanded={navCollapse}
+            onClick={toggleCollapse}
+            className={classnames("nav-toggler nav-toggler",{
+                toggled: navCollapse,
+            })}
+          >
+            <span className="navbar-toggle 1"/>
+            <span className="navbar-toggle 2"/>
+            <span className="navbar-toggle 3"/>
+          </button>
+        </div>
+        <Collapse
+          className="content-end"
+          navbar
+          isOpen={navCollapse}
+        >
+          <Nav navbar>
+            <NavItem>
+              <NavLink to="/home" tag={Link}>
+                <i className="navbar home-layout"/> /home
+              </NavLink>
+            </NavItem>
+
+            <NavItem>
+              <NavLink to="/work" tag={Link}>
+                <i className="navbar work-layout"/> /work
+              </NavLink>
+            </NavItem>
+
+            <NavItem>
+              <NavLink to="/about" tag={Link}>
+                <i className="navbar about-layout"/> /about
+              </NavLink>
+            </NavItem>
+
+            <NavItem>
+              <NavLink to="/contact" tag={Link}>
+                <i className="navbar contact-layout"/> /contact
+              </NavLink>
+            </NavItem>
+
+            <NavItem>
+              <NavLink
+              data-placement="bottom"
+              href="https://instagram.com"
+              target="_blank"
+              title="Follow me on Instagram"
+              >
+                <i className="nav insta"/>
+                <p className="nav placeholder-insta">Instagram</p>
+              </NavLink>
+            </NavItem>
+
+            <NavItem>
+              <NavLink
+              data-placement="bottom"
+              href="https://linkedin.com"
+              target="_blank"
+              title="Follow me on LinkedIn"
+              >
+                <i className="nav LinkedIn"/>
+                <p className="nav placeholder-LinkedIn">LinkedIn</p>
+              </NavLink>
+            </NavItem>
+
+            <NavItem>
+              <NavLink
+              data-placement="bottom"
+              href="https://github.com"
+              target="_blank"
+              title="Check out my projects repo"
+              >
+                <i className="nav Git"/>
+                <p className="nav placeholder-Git">GitHub</p>
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>        
+      </Container>
+    </NavBar>
+  );
+}
+
+
+
+
+export default NavBar;
