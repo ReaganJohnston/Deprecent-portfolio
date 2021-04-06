06fbf81a827e70fcdb54d1e9a8a5da11e5f34657
ReaganJohnston <hyerx01@gmail.com>
2021-04-06 15:05:19 +1000

    Updated About me sections

---
+++ portfolio/src/pages/about.js
@@ -0,0 +1,64 @@
+/*
+=========================================================
+* Coded by Reagan Johnston
+=========================================================
+*/
+
+import React from "react";
+
+const imgStyle = {
+  width: '98vw',
+  height: '30vw',
+}
+
+function AboutSection(){
+  return(
+    <>
+    <div className="bodyFrameworks"
+      style={imgStyle}>
+      <div className="card-description-header">
+        <h3>MY WORK</h3>
+      </div>
+      <div className="card-description-body">
+        <h4> I was never the best at school or academics, I'm just a super curious guy. This website exists to share and motivate me to continue
+              developing my ideas. As it documents my projects and challenges, I hope in the near future I would have more abstract knowledge to continue
+              creating crazier projects. A recent interest of mine was cyber-securiy. Cryptography, enumeration, Privilege Escalation and Pen-testing are all very interesting apsects
+              to research into. Continuing foward I can't wait to delve deaper, and apply my knowledge for further projects. 
+        </h4>
+      </div>
+      <div className="row-listPanel">
+        <div class="col-lg">
+          <h5> Software Development</h5>
+          <ul>
+            <li>Front end development. Animated, styled and appropriated based on target audiences</li>
+            <li>MVP testing utilising prototyping frameworks</li>
+            <li>Java/mySQL server-side logic, automation</li>
+            <li>GUI builds; Java, C, ReactJS</li>
+          </ul>
+        </div>
+
+        <div class="col-lg">
+          <h5> Cyber Security</h5>
+          <ul>
+            <li>Offensive Pentesting: nmap, ncat, metasploit, Hydra</li>
+            <li>Basic Privilege Escalation</li>
+            <li>Pentesting, encapsulation and metasploiting on reaganjohnston.com</li>
+          </ul>
+        </div>
+
+        <div class="col-lg">
+          <h5> Electronics</h5>
+          <ul>
+            <li>Retro-based Raspberri Pi emulators</li>
+            <li>Teensy controller interfaces</li>
+            <li>Automated printing modules. Created methods for more than one user to upload documents onto a server and print the documents anywhere.</li>
+          </ul>
+        </div>
+
+      </div>
+    </div>   
+    </>
+  );
+}
+
+export default AboutSection;
--- portfolio/src/pages/home.js
+++ portfolio/src/pages/home.js
@@ -7,44 +7,16 @@
 import React from "react";
 import NavigationBar from "./../components/navbar.js";
 import Header from "./../components/header.js"
-
-const imgStyle = {
-  width: '98vw',
-  height: '49vw',
-}
+import AboutSection from "./../pages/about.js"
 
 function HomePage(){
   return(
     <>
-    <NavigationBar/>
-    <div class="card-header">
-      <Header />
-    </div>
-
-    <div class="body-card">
-      <div className="bodyFrameworks"
-        style={imgStyle}>
-        <div className="card-description-header">
-          <h3>Hello! my name is Reagan Johnston, I’m a software developer and cyber-security analyst.</h3>
-        </div>
-        <div className="card-description-body">
-          <h4> I am passionate about building excellent software and applying my current cyber-security knowledge in the world around me.
-            Over the course of three years, I have learnt many skills, applications, frameworks and enumeration techniques to 
-            develop myself and satisify my inherent curiousity.
-          </h4>
-        </div>
-
+      <NavigationBar/>
+      <div class="card-header">
+        <Header />
       </div>
-    </div>
-
-    
-    <div class="Full-stack">
-
-    </div>
-    <div class= "Cyber-security">
-      
-    </div>
-    
+      <AboutSection/>
     </>
   );
 }
