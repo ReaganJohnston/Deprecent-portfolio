698f9552dbfc69d1f392e428e99819e6f62fa9cb
ReaganJohnston <hyerx01@gmail.com>
2021-04-08 17:05:07 +1000

    Finished Projects section 1

--- portfolio/src/pages/projects.js
+++ portfolio/src/pages/projects.js
@@ -21,33 +21,32 @@ function Projects(){
             <h4>Projects involved</h4>
           </div>
           <table class="table">
-            <thread>
               <tr>
                 <th>Title</th>
                 <th>Description</th>
                 <th>Language</th>
               </tr>
-            </thread>
-            <tr>
-              <td>reaganjohnston.com</td>
-              <td>Fullstack website to visualise my current projects</td>
-              <td>ReactJS, Express, Vercel (Now), node.js, npm</td>
-            </tr>
+            <tbody>
+              <tr>
+                <td>reaganjohnston.com</td>
+                <td>Fullstack website to visualise my current projects</td>
+                <td>ReactJS, Express, Vercel (Now), node.js, npm</td>
+              </tr>
 
-            <tr>
-              <td>johnstonorganics.com</td>
-              <td>Fullstack website for the purpose of showcasing the organic meat I'm selling on the side. For 
-                all intended purposes will become an e-commerce website later
-              </td>
-              <td>ReactJS, Express, Vercel (Now), node.js, npm</td>
-            </tr>
+              <tr>
+                <td>johnstonorganics.com</td>
+                <td>Fullstack website for the purpose of showcasing the organic meat I'm selling on the side. For 
+                  all intended purposes will become an e-commerce website later
+                </td>
+                <td>ReactJS, Express, Vercel (Now), node.js, npm</td>
+              </tr>
 
-            <tr>
-              <td>Raspberri Pi printing-hub</td>
-              <td>Online server to upload documents from anywhere for printing at a designated location</td>
-              <td>Python, Google-workspace API</td>
-            </tr>
-
+              <tr>
+                <td>Raspberri Pi printing-hub</td>
+                <td>Online server to upload documents from anywhere for printing at a designated location</td>
+                <td>Python, Google-workspace API</td>
+              </tr>
+            </tbody>
           </table>
         </div>
     </div>
